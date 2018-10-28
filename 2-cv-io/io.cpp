5198aad79dd3ad0f7d098ec7ae143cf5cfc3d9fb
Darien Martinez Torres <darien.martinez@gmail.com>
2018-10-28 05:21:12 +0000

    Episode 2 : Multimedia IO

---
+++ 2-cv-io/io.cpp
@@ -0,0 +1,50 @@
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+int main() 
+{
+    cv::Mat img = cv::imread("lena.jpg");
+    
+    
+    
+    // Step 1: just showing the image
+    // cv::imshow("img", img);
+
+    // Step 2 and 3: Sobel lena and save it to disk.
+    // cv::Mat dst;
+    // cv::Sobel(img, dst, CV_32F, 1, 1);
+    
+    //cv::imshow("img", dst / 256 );
+    
+    //cv::imwrite("lena_sobel.png", dst);
+    //cv::waitKey();
+
+    // Step 3: Video processing and saving a video
+
+    cv::Mat dst;
+
+    cv::VideoCapture input("cars.mp4");
+
+    cv::VideoWriter output(
+	"car_sobel.avi",
+	CV_FOURCC('X', 'V', 'I', 'D'),
+	30,
+	cv::Size(input.get(CV_CAP_PROP_FRAME_WIDTH),
+		 input.get(CV_CAP_PROP_FRAME_HEIGHT)));
+
+    for(;;)
+    {
+	if (!input.read(img))
+	    break;
+	
+	cv::Sobel(img, dst, CV_8U, 1, 1);
+
+	output.write(dst);
+
+	cv::imshow("img", dst);
+	char c = cv::waitKey(30);
+
+	if (c == ' ')
+	    break;
+    }
+}
